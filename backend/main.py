6b1da87ecb8100273066f1901b148e2cee2c620f
johanseb15 <sebasmol2010@gmail.com>
2025-08-05 10:09:27 -0300

    Commit backend

--- backend/main.py
+++ backend/main.py
@@ -509,7 +509,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=int(os.getenv("PORT", 8000)),
+        port=int(os.getenv("PORT", "8000")),
         reload=True if os.getenv("ENVIRONMENT") == "development" else False,
         log_level="info"
     )
